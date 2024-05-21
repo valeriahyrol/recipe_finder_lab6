343d892737fcad052a56c0a85d42f263defb9c6b
ValeriaHyrol <ipz221_gvi@student.ztu.edu.ua>
2024-05-21 20:54:15 +0300

    Added connection class

---
+++ lab6/Classes/db.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace lab6.Classes
+{
+    public class db
+    {
+        private MySqlConnection connection;
+
+        public db(string connectionString)
+        {
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
+

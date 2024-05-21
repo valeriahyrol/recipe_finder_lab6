ad60b04ba04bc5e34a2cbb3bd87e46c3f39cc28d
ValeriaHyrol <ipz221_gvi@student.ztu.edu.ua>
2024-05-21 20:44:37 +0300

    Added form for searching recipes

---
+++ lab6/SearchForm.cs
@@ -0,0 +1,92 @@
+﻿using lab6.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab6
+{
+    public partial class SearchForm : Form
+    {
+        private RecipeRepository repository;
+
+        public SearchForm()
+        {
+            InitializeComponent();
+            repository = RecipeRepository.Instance;
+            SearchButton.Click += SearchButton_Click;
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string query = SearchTextBox.Text;
+
+            if (!IsValidQuery(query))
+            {
+                MessageBox.Show("Invalid input. Please enter a valid search query.");
+                return;
+            }
+
+            ISearchStrategy strategy = GetSearchStrategy();
+
+            if (strategy == null)
+            {
+                MessageBox.Show("Please select a search criteria.");
+                return;
+            }
+
+            List<Recipe> results = repository.SearchRecipes(strategy, query);
+            DisplayResults(results);
+        }
+
+        private bool IsValidQuery(string query)
+        {
+            return !string.IsNullOrEmpty(query) && query.All(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c));
+        }
+
+        private ISearchStrategy GetSearchStrategy()
+        {
+            if (SearchByNameRadioButton.Checked)
+            {
+                return new SearchByNameStrategy();
+            }
+            if (SearchByIngredientsRadioButton.Checked)
+            {
+                return new SearchByIngredientsStrategy();
+            }
+            if (SearchByIdRadioButton.Checked)
+            {
+                return new SearchByIdStrategy();
+            }
+
+            return null;
+        }
+
+        private void DisplayResults(List<Recipe> recipes)
+        {
+            if (recipes.Count == 0)
+            {
+                MessageBox.Show("No recipes found.");
+            }
+            else
+            {
+                foreach (var recipe in recipes)
+                {
+                    MessageBox.Show($"Recipe: {recipe.Title}, Ingredients: {recipe.Text}");
+                }
+            }
+
+            ResultsDataGridView.DataSource = recipes;
+        }
+
+        private void SearchByIngredientsRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

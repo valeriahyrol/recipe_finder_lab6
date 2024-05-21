53b9e8484da0c77477de5d7b6fe8c3e56a579c5b
ValeriaHyrol <ipz221_gvi@student.ztu.edu.ua>
2024-05-21 20:49:32 +0300

    Added strategy class to search recipe by it`s name

---
+++ lab6/Classes/SearchByNameStrategy.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6.Classes
+{
+    public class SearchByNameStrategy : BaseSearchStrategy
+    {
+        public override List<Recipe> Search(db database, string query)
+        {
+            string sqlQuery = "SELECT * FROM lab6 WHERE title LIKE @query";
+            return ExecuteSearch(database, sqlQuery, cmd => cmd.Parameters.AddWithValue("@query", "%" + query + "%"));
+        }
+    }
+}

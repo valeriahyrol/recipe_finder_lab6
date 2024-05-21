dd9b6326bf40e81172b97233dab03b6bfa194a64
ValeriaHyrol <ipz221_gvi@student.ztu.edu.ua>
2024-05-21 20:47:52 +0300

    Added the interface for search

---
+++ lab6/Classes/ISearchStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6.Classes
+{
+    public interface ISearchStrategy
+    {
+        List<Recipe> Search(db database, string query);
+    }
+
+}
